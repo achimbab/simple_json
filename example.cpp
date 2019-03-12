693cafcccbe788d11737027192cad64b1fee510d
achimbab <07c00h@gmail.com>
2019-03-12 23:21:04 +0900

    fix example

--- example.cpp
+++ example.cpp
@@ -71,6 +71,41 @@ public:
     int magReg;
     int phyReg;
 
+    Stats() : 
+        strength(0),
+        vitality(0),
+        intelligence(0),
+        magReg(0),
+        phyReg(0) {
+    }
+
+    Stats(const Stats& other) : 
+        strength(other.strength),
+        vitality(other.vitality),
+        intelligence(other.intelligence),
+        magReg(other.magReg),
+        phyReg(other.phyReg) {
+    }
+
+    Stats& operator+=(const Stats& rhs) {
+        strength += rhs.strength;
+        vitality += rhs.vitality;
+        intelligence += rhs.intelligence;
+        magReg += rhs.magReg;
+        phyReg += rhs.phyReg;
+        return *this;
+    }
+
+    Stats operator+(const Stats& rhs) {
+        Stats stats(*this);
+        stats.strength += rhs.strength;
+        stats.vitality += rhs.vitality;
+        stats.intelligence += rhs.intelligence;
+        stats.magReg += rhs.magReg;
+        stats.phyReg += rhs.phyReg;
+        return stats;
+    }
+
     friend ostream& operator<<(ostream& os, const Stats& s) {
         os << "S:" << s.strength << ", V:" << s.vitality << ", I:" << s.intelligence << ", M:" << s.magReg << ", P:" << s.phyReg;
         return os;
@@ -81,21 +116,21 @@ class Equip {
 public:
     equip_part_t part;
     string set;
-    Stats stat;
+    Stats stats;
 
     Equip(JSONValue v) {
         auto o = v.v_.o_;
         set = o->find("Set")->second.v_.s_;
         part = fromString(o->find("Part")->second.v_.s_);
-        stat.strength = o->find("Strength")->second.v_.i_;
-        stat.vitality = o->find("Vitality")->second.v_.i_;
-        stat.intelligence = o->find("Intelligence")->second.v_.i_;
-        stat.magReg = o->find("MagReg")->second.v_.i_;
-        stat.phyReg = o->find("PhyReg")->second.v_.i_;
+        stats.strength = o->find("Strength")->second.v_.i_;
+        stats.vitality = o->find("Vitality")->second.v_.i_;
+        stats.intelligence = o->find("Intelligence")->second.v_.i_;
+        stats.magReg = o->find("MagReg")->second.v_.i_;
+        stats.phyReg = o->find("PhyReg")->second.v_.i_;
     }
 
-    friend ostream& operator<<(ostream& os,  const Equip& e) {
-        os << e.set << ", " << e.part << ", " << e.stat;
+    friend ostream& operator<<(ostream& os, const Equip& e) {
+        os << e.set << ", " << e.part << ", " << e.stats;
         return os;
     }
 };
@@ -103,7 +138,34 @@ public:
 class SetOption {
 public:
     string set;
-    Stats* stats[5];
+    Stats stats[5];
+    int no;
+
+    SetOption(JSONValue v) : no(0) {
+        auto o = v.v_.o_;
+        set = o->find("Set")->second.v_.s_;
+        memset(&stats, 0, sizeof(stats));
+        add(v);
+    }
+
+    void add(JSONValue v) {
+        auto o = v.v_.o_;
+        int count = o->find("Count")->second.v_.i_;
+        stats[count - 1].strength = o->find("Strength")->second.v_.i_;
+        stats[count - 1].vitality = o->find("Vitality")->second.v_.i_;
+        stats[count - 1].intelligence = o->find("Intelligence")->second.v_.i_;
+        stats[count - 1].magReg = o->find("MagReg")->second.v_.i_;
+        stats[count - 1].phyReg = o->find("PhyReg")->second.v_.i_;
+        no = max(count, no);
+    }
+
+    friend ostream& operator<<(ostream& os, const SetOption& o) {
+        os << o.set << endl;
+        for (int i = 0; i < o.no; i++) {
+            os << o.stats[i] << endl;
+        }
+        return os;
+    }
 };
 
 int combine_equips() {
@@ -128,7 +190,7 @@ int combine_equips() {
     print(value, 0);
 
     vector<Equip> equips;
-    vector<SetOption> setoptions;
+    map<string, SetOption> setOptions;
 
     // Load equipments
     auto equipsSource = value.v_.o_->find("Equipments");
@@ -144,7 +206,55 @@ int combine_equips() {
     }
 
     // Load setoptions
+    auto setOptionsSource = value.v_.o_->find("SetOptions");
+    if (setOptionsSource == value.v_.o_->end()) {
+        return 1;
+    }
+    for (auto o : *(setOptionsSource->second.v_.a_)) {
+        string set = o.v_.o_->find("Set")->second.v_.s_;
+        auto s = setOptions.find(set);
+        if (s == setOptions.end()) {
+            setOptions.insert(make_pair(set, SetOption(o)));
+        } else {
+            s->second.add(o);
+        }
+    }
 
+    for (auto o : setOptions) {
+        cout << o.second << endl;
+    }
+
+    // combine
+    vector<vector<Equip>> ec;
+    // body
+    for (auto b : equips) {
+        if (b.part == BODY) {
+            for (auto h : equips) {
+                if (h.part == HEAD) {
+                    for (auto g : equips) {
+                        if (g.part == GLOVES) {
+                            for (auto w : equips) {
+                                if (w.part == GLOVES) {
+                                    for (auto s : equips) {
+                                        if (s.part == GLOVES) {
+                                            Stats total = b.stats + h.stats + g.stats + w.stats + s.stats;
+                                            cout << "\nTOTAL: " << total << endl;
+                                            cout << b << endl  << h << endl << g << endl << w << endl << s << endl;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    // head
+    // gloves
+    // waist
+    // shoes
 
     return 0;
 }
